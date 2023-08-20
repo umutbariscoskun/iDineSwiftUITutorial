80a96b4a94083102b0a169f1fffd77a568ae2ab5
umutbariscoskun <umutcoskun2017@gmail.com>
2023-08-20 20:34:44 +0300

    add order view

---
+++ iDineSwiftUITutorial/OrderView.swift
@@ -0,0 +1,44 @@
+//
+//  OrderView.swift
+//  iDineSwiftUITutorial
+//
+//  Created by Umut Barış Çoşkun on 20.08.2023.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    var body: some View {
+        NavigationStack{
+            List{
+                Section{
+                    ForEach(order.items){
+                        item in
+                        HStack{
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                        
+                        
+                    }
+                }
+                Section{
+                    NavigationLink("Place Order"){
+                       CheckoutView()
+                    }
+                }
+                
+            }.navigationTitle("Order")
+            
+        }
+    }
+    
+    struct OrderView_Previews: PreviewProvider {
+        static var previews: some View {
+            OrderView()
+                .environmentObject(Order())
+        }
+    }
+}
