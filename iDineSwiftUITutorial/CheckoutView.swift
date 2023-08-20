e4ede7cae6a396aea5cfa60ca6e02c2359388bfc
umutbariscoskun <umutcoskun2017@gmail.com>
2023-08-20 20:34:55 +0300

    add checkout view

---
+++ iDineSwiftUITutorial/CheckoutView.swift
@@ -0,0 +1,73 @@
+//
+//  CheckoutView.swift
+//  iDineSwiftUITutorial
+//
+//  Created by Umut Barış Çoşkun on 20.08.2023.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    
+    let paymentTypes = ["Cash", "Credit Card", "Points"]
+    @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetailts = false
+    @State private var loyaltyNumber = ""
+    
+    let tipAmounts = [10,15,20,25,0]
+    @State private var tipAmount = 15
+    
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
+    var body: some View {
+        Form{
+            Section{
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self){
+                        Text($0)
+                        
+                    }
+                }
+                Toggle("Add loyalty card", isOn: $addLoyaltyDetailts.animation())
+                if addLoyaltyDetailts {
+                    TextField("Enter your ID", text: $loyaltyNumber)
+                }
+                
+                 
+            }
+            
+            Section("Add a tip?"){
+                Picker("Percentage: ", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self){
+                        Text("\($0)%")
+                        
+                    }
+                }.pickerStyle(.segmented)
+            }
+            
+            Section("Total \(totalPrice)"){
+                Button("Confirm Order"){
+                    //place the order
+                }
+            }
+            
+           
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView()
+            .environmentObject(Order())
+    }
+}
