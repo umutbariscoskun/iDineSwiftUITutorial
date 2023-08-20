1a9d73fd0d7e2ef7206cf524f3e3a9a3ea3b1305
umutbariscoskun <umutcoskun2017@gmail.com>
2023-08-20 16:17:51 +0300

    Initial Commit

---
+++ iDineSwiftUITutorial/iDineSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineSwiftUITutorialApp.swift
+//  iDineSwiftUITutorial
+//
+//  Created by Umut Barış Çoşkun on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
